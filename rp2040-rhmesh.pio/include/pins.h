95238553963b7e62d753ad70bfb043367c377743
theMladyPan <stanislav@rubint.sk>
2022-07-28 11:52:23 +0200

    neuspesny pokus

---
+++ rp2040-rhmesh.pio/include/pins.h
@@ -0,0 +1,12 @@
+#define RF_MOSI     20 
+#define RF_MISO     21
+#define RF_SCK      22
+#define RF_NSS      23
+#define RF_RESET    24
+#define RF_DI0      19
+#define RF_DI1      18
+
+#define PIN_SPI_MOSI    RF_MOSI
+#define PIN_SPI_MISO    RF_MISO
+#define PIN_SPI_SCK     RF_SCK
+#define PIN_SPI_SS      RF_NSS
